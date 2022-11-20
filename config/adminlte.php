5ec6898798c18013a273deb8b1c41938a3dced1e
yhws26 <116448494+yhws26@users.noreply.github.com>
2022-11-20 18:08:04 -0400

    cambio lenguaje + logo

--- config/adminlte.php
+++ config/adminlte.php
@@ -109,7 +109,7 @@ return [
     'preloader' => [
         'enabled' => true,
         'img' => [
-            'path' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+            'path' => 'vendor/adminlte/dist/img/sus.png',
             'alt' => 'AdminLTE Preloader Image',
             'effect' => 'animation__shake',
             'width' => 60,
